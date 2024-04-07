f4fe45ac0630af9ccec33dda5155616d7823aa5b
bartosz_s <bartosz.starzec@websensa.com>
2024-04-07 16:01:54 +0200

    switch to fiber, add templ, add unit login view, add unit test

--- cmd/main.go
+++ cmd/main.go
@@ -1,158 +1,50 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"html/template"
-	"io"
+	"fmt"
+	"github.com/1337Bart/improve-yourself/routes"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/joho/godotenv"
 )
 
-type Templates struct {
-	templates *template.Template
-}
-
-// Render renders the given template with the provided data to the given io.Writer.
-// It takes the name of the template, the data to be rendered, and the echo.Context as parameters.
-// It returns an error.
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func NewTemplate() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("../views/*.html")),
-	}
-}
-
-type Block struct {
-	Id int
-}
-
-type Blocks struct {
-	Start  int
-	Next   int
-	More   bool
-	Blocks []Block
-}
-
-type Count struct {
-	Count int
-}
-
-type Contact struct {
-	Name  string
-	Email string
-}
-
-func newContact(name, email string) Contact {
-	return Contact{
-		Name:  name,
-		Email: email,
-	}
-}
-
-type Contacts = []Contact
-
-func (d *Data) hasEmail(email string) bool {
-	for _, contact := range d.Contacts {
-		if contact.Email == email {
-			return true
-		}
-	}
-	return false
-}
-
-type Data struct {
-	Contacts Contacts
-}
-
-func newData() Data {
-	return Data{
-		Contacts: []Contact{
-			newContact("John", "jd@gmail,com"),
-			newContact("Jane", "jd@gmail,com"),
-		},
-	}
-}
-
-type FormData struct {
-	Values map[string]string
-	Errors map[string]string
-}
-
-func newFormData() FormData {
-	return FormData{
-		Values: make(map[string]string),
-		Errors: make(map[string]string),
-	}
-}
-
-type Page struct {
-	Data Data
-	Form FormData
-}
-
-func newPage() Page {
-	return Page{
-		Data: newData(),
-		Form: newFormData(),
-	}
-}
-
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
+	env := godotenv.Load()
+	if env != nil {
+		panic("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":4000"
+	} else {
+		port = ":" + port
+	}
 
-	page := newPage()
-	e.Renderer = NewTemplate()
-
-	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", page)
+	app := fiber.New(fiber.Config{
+		IdleTimeout: 5 * time.Second,
 	})
 
-	e.POST("/contacts", func(c echo.Context) error {
-		name := c.FormValue("name")
-		email := c.FormValue("email")
+	app.Use(compress.New())
 
-		if page.Data.hasEmail(email) {
-			formData := newFormData()
-			formData.Values["name"] = name
-			formData.Values["email"] = email
-			formData.Errors["email"] = "Email already exists"
+	routes.SetRoutes(app)
 
-			return c.Render(422, "form", formData)
+	go func() {
+		if err := app.Listen(port); err != nil {
+			log.Panic(err)
 		}
+	}()
 
-		contact := newContact(name, email)
-		page.Data.Contacts = append(page.Data.Contacts, contact)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-		c.Render(200, "form", newFormData())
-		return c.Render(200, "oob-contact", contact)
-	})
+	<-c
 
-	//e.GET("/blocks", func(c echo.Context) error {
-	//	startStr := c.QueryParam("start")
-	//	start, err := strconv.Atoi(startStr)
-	//	if err != nil {
-	//		start = 0
-	//	}
-	//
-	//	blocks := []Block{}
-	//	for i := start; i < start+10; i++ {
-	//		blocks = append(blocks, Block{Id: i})
-	//	}
-	//
-	//	template := "blocks"
-	//	if start == 0 {
-	//		template = "blocks-index"
-	//	}
-	//	return c.Render(http.StatusOK, template, Blocks{
-	//		Start:  start,
-	//		Next:   start + 10,
-	//		More:   start+10 < 100,
-	//		Blocks: blocks,
-	//	})
-	//})
-
-	e.Logger.Fatal(e.Start(":42069"))
+	app.Shutdown()
+	fmt.Println("Shutting down server")
 }
---
+++ routes/routes.go
@@ -0,0 +1,53 @@
+package routes
+
+import (
+	"github.com/1337Bart/improve-yourself/views"
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+)
+
+func render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
+	componentHandler := templ.Handler(component)
+	for _, o := range options {
+		o(componentHandler)
+	}
+	return adaptor.HTTPHandler(componentHandler)(c)
+}
+
+type loginForm struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+type settingsForm struct {
+	Amount   int  `form:"amount"`
+	SearchOn bool `form:"searchOn"`
+	AddNew   bool `form:"addNew"`
+}
+
+func SetRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return render(c, views.Home())
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		input := settingsForm{}
+		if err := c.BodyParser(&input); err != nil {
+			return c.SendString("<h2>Error: Something went wrong</h2>")
+		}
+		return c.SendStatus(200)
+	})
+
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return render(c, views.Login())
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		input := loginForm{}
+		if err := c.BodyParser(&input); err != nil {
+			return c.SendString("<h2>Error: Something went wrong</h2>")
+		}
+		return c.SendStatus(200)
+	})
+}
